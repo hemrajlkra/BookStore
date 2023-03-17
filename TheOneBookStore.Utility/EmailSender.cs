e07225c30007a9c96f6a14a96865e54d617dfea3
Hemraj Lakhara <60054824+hemrajlkra@users.noreply.github.com>
2023-03-17 10:14:33 +0530

    Update EmailSender.cs

--- TheOneBookStore.Utility/EmailSender.cs
+++ TheOneBookStore.Utility/EmailSender.cs
@@ -15,7 +15,7 @@ namespace TheOneBookStore.Utility
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
             var emailToSend = new MimeMessage();
-            emailToSend.From.Add(MailboxAddress.Parse("honeylakhara91111@gmail.com"));
+            emailToSend.From.Add(MailboxAddress.Parse("your_Email@gmail.com"));
             emailToSend.To.Add(MailboxAddress.Parse(email));
             emailToSend.Subject = subject;
             emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html){Text = htmlMessage};
@@ -24,7 +24,7 @@ namespace TheOneBookStore.Utility
 			using (var emailClient = new SmtpClient())
             {
                 emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
-                emailClient.Authenticate("honeylakhara91111@gmail.com","fckdjspjjbmfmdzf");
+                emailClient.Authenticate("your_Email@gmail.com","16 digit app code");
                 emailClient.Send(emailToSend);
                 emailClient.Disconnect(true);
             }
